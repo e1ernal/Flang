665bb9de9ad116138dc4780f63945f0675a87b68
e1ernal <goodnightlemon@gmail.com>
2025-05-21 11:46:29 +0300

    Initial Commit

---
+++ Flang/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Flang
+//
+//  Created by e1ernal on 21.05.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
