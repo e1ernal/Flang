fe2cba734be127f0ba43bb8e53a662e47fb5813d
e1ernal <goodnightlemon@gmail.com>
2025-05-24 03:59:56 +0300

    Input Source monitoring using Notification Center

--- Flang/AppDelegate.swift
+++ Flang/AppDelegate.swift
@@ -8,127 +8,44 @@
 import Cocoa
 import Carbon
 
-class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusBar: NSStatusBar!
-    private var statusBarItem: NSStatusItem!
+final class AppDelegate: NSObject, NSApplicationDelegate, InputSourceMonitoring {
+    // MARK: - Public Properties
     
-    private var sources = Sources()
-    private var timer: Timer?
-    private var showInputSourceName: Bool {
+    // MARK: - Private Properties
+    private var statusBar: NSStatusBar
+    private var statusBarItem: NSStatusItem
+    
+    private var inputSource: InputSource
+    
+    private var showInputSourceName: Bool = false {
         didSet {
-            configureMenu()
             UserDefaults.standard.set(showInputSourceName, forKey: "showInputSourceName")
         }
     }
     
+    // MARK: - Initialization
     override init() {
-        showInputSourceName = UserDefaults.standard.bool(forKey: "showInputSourceName")
+        statusBar = NSStatusBar.system
+        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+        inputSource = InputSource()
+        super.init()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Application Lifecycle
-    
+    // MARK: - Lifecycle
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        inputSource.delegate = self
+        showInputSourceName = UserDefaults.standard.bool(forKey: "showInputSourceName")
+    }
     func applicationDidFinishLaunching(_ notification: Notification) {
-        configureStatusBarItem()
-        configureInputSourceMonitoring()
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        timer?.invalidate()
-    }
-    
-    // MARK: - Configuration
-    
-    private func configureStatusBarItem() {
-        statusBar = NSStatusBar.system
-        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
-        
-        configureMenu()
-    }
-    
-    private func configureMenu(){
-        let menu = NSMenu()
-        
-        let allSources = InputSourceHelper.getAllInputSources()
-        for source in allSources {
-            let item = NSMenuItem(
-                title: source.name,
-                action: nil,
-                keyEquivalent: ""
-            )
-            
-            item.target = self
-            let image = NSImage(named: "Flags/ru")
-            image?.size = CGSize(width: 32.3, height: 16.15)
-            item.image = image
-            item.state = .on
-            menu.addItem(item)
-        }
-        
-        // Show Input Source Name
-        let showInputSourceNameItem = NSMenuItem(
-            title: (showInputSourceName ? "Hide" : "Show") + " Input Source Name",
-            action: #selector(showInputSourceNamePressed),
-            keyEquivalent: ""
-        )
-        
-        // Quit
-        let quitItem = NSMenuItem(
-            title: "Quit Flang",
-            action: #selector(quitPressed),
-            keyEquivalent: "q"
-        )
-        
-        // Targets
-        showInputSourceNameItem.target = self
-        quitItem.target = self
-        
-        // Add Items
-        menu.addItem(.separator())
-        menu.addItem(showInputSourceNameItem)
-        menu.addItem(.separator())
-        menu.addItem(quitItem)
-        
-        statusBarItem.menu = menu
-    }
-    
-    private func configureInputSourceMonitoring() {
-        timer = Timer.scheduledTimer(
-            timeInterval: 0.5,
-            target: self,
-            selector: #selector(updateStatusBarItem),
-            userInfo: nil,
-            repeats: true
-        )
+        configureStatusBarItemButton()
+        configureStatusBarItemMenu()
     }
     
     // MARK: - Actions
-    
-    @objc
-    private func updateStatusBarItem() {
-        guard
-            let currentInputSource = TISCopyCurrentKeyboardInputSource()?.takeRetainedValue(),
-            let sourceID = TISGetInputSourceProperty(currentInputSource, kTISPropertyInputSourceID)
-        else { return }
-        
-        let fullId = Unmanaged<CFString>.fromOpaque(sourceID).takeUnretainedValue() as String
-        let id = String(fullId.split(separator: ".").last ?? "")
-        let source = sources.get(id)
-        let image = NSImage(named: "Flags/\(source.flagCode)")
-        image?.size = CGSize(width: 32.3, height: 16.15)
-        let title = showInputSourceName ? source.nameShort : ""
-        
-        DispatchQueue.main.async {
-            self.statusBarItem.button?.title = title
-            self.statusBarItem.button?.image = image
-        }
-    }
-    
-    // MARK: - Menu Actions
-    
     @objc
     private func showInputSourceNamePressed() {
         showInputSourceName.toggle()
@@ -138,5 +55,64 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private func quitPressed() {
         NSApplication.shared.terminate(nil)
     }
+    
+    // MARK: - Public Methods
+    func inputSourceDidChange() {
+        statusBarItem.button?.title = inputSource.name
+    }
+    
+    // MARK: - Private Methods
+    private func configureStatusBarItemButton() {
+        statusBarItem.button?.title = inputSource.name
+    }
+    
+    private func configureStatusBarItemMenu(){
+        let menu = NSMenu()
+//        
+//        let allSources = InputSourceHelper.getAllInputSources()
+//        for source in allSources {
+//            let item = NSMenuItem(
+//                title: source.name,
+//                action: nil,
+//                keyEquivalent: ""
+//            )
+//            print(source)
+//            print(sources.get(source.id))
+//            item.target = self
+//            let image = NSImage(named: "Flags/ru")
+//            image?.size = CGSize(width: 32.3, height: 16.15)
+//            item.image = image
+//            item.state = .on
+//            menu.addItem(item)
+//        }
+//        
+//        // Show Input Source Name
+//        let showInputSourceNameItem = NSMenuItem(
+//            title: (showInputSourceName ? "Hide" : "Show") + " Input Source Name",
+//            action: #selector(showInputSourceNamePressed),
+//            keyEquivalent: ""
+//        )
+//        
+        // Quit
+        let quitItem = NSMenuItem(
+            title: "Quit Flang",
+            action: #selector(quitPressed),
+            keyEquivalent: "q"
+        )
+//        
+//        // Targets
+//        showInputSourceNameItem.target = self
+//        quitItem.target = self
+//        
+//        // Add Items
+//        menu.addItem(.separator())
+//        menu.addItem(showInputSourceNameItem)
+//        menu.addItem(.separator())
+        menu.addItem(quitItem)
+//        
+        statusBarItem.menu = menu
+    }
+    
+    // MARK: - Deinitialization
+    deinit { print("Deinit \(String(describing: AppDelegate.self))") }
 }
-
--- Flang/Sources/Models/Source.swift
+++ Flang/Sources/Models/Source.swift
@@ -5,20 +5,27 @@
 //  Created by e1ernal on 21.05.2025.
 //
 
+import Foundation
+
 struct Source: Decodable {
     let name: String
     let nameShort: String
     let nameNative: String
     let nameNativeShort: String
+    
     let flagEmoji: String
     let flagCode: String
+    let flagImage: Data?
     
     private enum CodingKeys: String, CodingKey {
         case name
         case nameShort = "name_short"
         case nameNative = "name_native"
         case nameNativeShort = "name_native_short"
+        
         case flagEmoji = "flag_emoji"
         case flagCode = "flag_code"
+        case flagImage = "flag_image"
     }
 }
+
